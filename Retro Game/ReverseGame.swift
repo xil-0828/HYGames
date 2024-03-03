49ffa6907b7615c2d4067d565e3e9110ea296924
yicode <120433484+yuki-845@users.noreply.github.com>
2024-03-04 01:31:22 +0900

    コミット

---
+++ Retro Game/ReverseGame.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Retro Game
+//
+//  Created by 平井悠貴 on 2024/03/03.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
