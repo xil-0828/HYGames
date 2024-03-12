2418e6d17ef2f1d3a81c29694f8387ceccee863f
yicode <120433484+yuki-845@users.noreply.github.com>
2024-03-13 01:21:40 +0900

    commit change

---
+++ Retro Game/Reverse/OnlineReversiGame.swift
@@ -0,0 +1,18 @@
+//
+//  OnlineReversiGame.swift
+//  Retro Game
+//
+//  Created by 平井悠貴 on 2024/03/13.
+//
+
+import SwiftUI
+
+struct OnlineReversiGame: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnlineReversiGame()
+}
