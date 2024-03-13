eb9a6e7695e628201991b44d0987e5cef077271a
yicode <120433484+yuki-845@users.noreply.github.com>
2024-03-14 01:51:51 +0900

    commit change

---
+++ Retro Game/Reverse/ReversiBattleDataClass.swift
@@ -0,0 +1,18 @@
+//
+//  ReversiBattleDataClass.swift
+//  Retro Game
+//
+//  Created by 平井悠貴 on 2024/03/14.
+//
+
+import SwiftUI
+
+struct ReversiBattleDataClass: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReversiBattleDataClass()
+}
