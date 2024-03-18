6df8f1e0b45b537a00da7ae8ccfd27e67ef341c3
yicode <120433484+yuki-845@users.noreply.github.com>
2024-03-19 01:01:01 +0900

    commit change

---
+++ Retro Game/SuikaGame/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  Retro Game
+//
+//  Created by 平井悠貴 on 2024/03/15.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
