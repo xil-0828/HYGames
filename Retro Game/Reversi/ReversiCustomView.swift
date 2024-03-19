c2952c718c6dcd0f324b78244e31e31794b4a489
yicode <120433484+yuki-845@users.noreply.github.com>
2024-03-20 01:30:43 +0900

    commit change

---
+++ Retro Game/Reversi/ReversiCustomView.swift
@@ -0,0 +1,18 @@
+//
+//  ReversiCustomView.swift
+//  Retro Game
+//
+//  Created by 平井悠貴 on 2024/03/20.
+//
+
+import SwiftUI
+
+struct ReversiCustomView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReversiCustomView()
+}
