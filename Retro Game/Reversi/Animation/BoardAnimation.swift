2c924bef35185d34fa28eebc7a6fad194577ded4
yicode <120433484+yuki-845@users.noreply.github.com>
2024-04-22 17:29:08 +0900

    commit change

---
+++ Retro Game/Reversi/Animation/BoardAnimation.swift
@@ -0,0 +1,18 @@
+//
+//  BoardAnimation.swift
+//  HYGames
+//
+//  Created by 平井悠貴 on 2024/04/22.
+//
+
+import SwiftUI
+
+struct BoardAnimation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BoardAnimation()
+}
---
+++ Retro Game/Reversi/Animation/TextAnimation.swift
@@ -0,0 +1,18 @@
+//
+//  TextAnimation.swift
+//  HYGames
+//
+//  Created by 平井悠貴 on 2024/04/22.
+//
+
+import SwiftUI
+
+struct TextAnimation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextAnimation()
+}
